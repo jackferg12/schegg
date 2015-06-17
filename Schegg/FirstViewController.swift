eb44177a1083cf5daa3bbba48f8ec408e332a865
Osei Poku <opoku@chegg.com>
2015-06-16 19:57:03 -0700

    Initial Commit

---
+++ Schegg/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Schegg
+//
+//  Created by Osei Poku on 6/16/15.
+//  Copyright (c) 2015 Chegg, Inc. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
